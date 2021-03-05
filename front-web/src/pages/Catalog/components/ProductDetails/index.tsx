423d3f1403ab5f7ee791618c8a07c055e638062a
JoelMaciel <soldado25461@gmail.com>
2021-03-05 16:56:25 -0300

    Iniciando layout

--- front-web/src/pages/Catalog/components/ProductDetails/index.tsx
+++ front-web/src/pages/Catalog/components/ProductDetails/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import {ReactComponent as ArrowIcon} from '../../../../core/assets/images/arrow.svg';
 import './styles.scss'
 
 type ParamsType = {
@@ -9,9 +10,15 @@ type ParamsType = {
 const ProductDetails =() =>{
     const {productId} = useParams<ParamsType>();
     console.log(productId);
+    
 return(
     <div className="product-details-container">
-        <h1>Product details</h1>
+        <div className="card-base  border-radius-20 product-details ">
+         <Link to ="/products" className="products-details-goback">
+            <ArrowIcon  className="icon-goback"/>   
+            <h1 className="text-goback">voltar</h1>
+         </Link>
+        </div>
     </div>
     
 );
