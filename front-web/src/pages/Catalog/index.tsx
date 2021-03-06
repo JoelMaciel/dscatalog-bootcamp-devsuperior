04b326eca3d8d30ec83e24537e03b84509e51455
JoelMaciel <soldado25461@gmail.com>
2021-03-06 18:45:08 -0300

    Request HTTP

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,13 +1,17 @@
 import Reacts , { useEffect } from  'react';
 import {Link} from 'react-router-dom';
+import { makeRequest } from '../../core/utils/request';
 import ProductCard from './components/ProductCard';
 import './styles.scss'
 
 const Catalog = () => {
-
     useEffect(() => {
-        fetch('http://localhost:3000/products')
-        .then(response => response.json())
+        const params = {
+            page : 0,
+            linesPerPage:12
+        }
+
+        makeRequest({url: '/products', params})
         .then(response => console.log(response));
 
     }, []);
