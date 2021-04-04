792a513b971625685138af371eee4526272982f1
JoelMaciel <soldado25461@gmail.com>
2021-04-04 03:11:35 -0300

    formularios ADM parte2

--- front-web/src/pages/Admin/components/Products/indes.tsx
+++ front-web/src/pages/Admin/components/Products/indes.tsx
@@ -1,27 +1,18 @@
 import React from 'react';
+import List from './List';
 import { Route, Switch } from 'react-router';
-import { Link } from 'react-router-dom';
-
+import Form from './Form';
 
 
 const Products = ()=> {
     return (
-        <div>
-         <Link to="/admin/products" className="mr-5">
-            Listar produtos
-         </Link>
-         <Link to="/admin/products/create" className="mr-5">
-            Criar produtos
-         </Link> 
-         <Link to="/admin/products/10" className="mr-5">
-            Editar produtos
-         </Link>    
+        <div>   
          <Switch>
           <Route path="/admin/products" exact>
-            <h1>Exibir a listagem de produtos</h1>
+            <List />
           </Route> 
           <Route path="/admin/products/create">
-            <h1>Criar novo produto</h1>
+            <Form />
           </Route> 
           <Route path="/admin/products/:productId">
             <h1>Editar um produto</h1>
