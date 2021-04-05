1b7af2828d2e5a8878669839f5fc4bc9ecd7b5b2
JoelMaciel <soldado25461@gmail.com>
2021-04-05 01:30:49 -0300

    formularios

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,3 +1,4 @@
+import { makeRequest } from 'core/utils/request';
 import React, { useState } from 'react'
 import BaseForm from '../../BaseForm';
 import './styles.scss';
@@ -6,18 +7,21 @@ type FormState = {
    name: string;
    price: string;
    category: string;
+   description:string
 }
+type FormEvent = React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>
 
 
 const Form = () => {
 
    const [formData, setFormData] = useState<FormState>({
-      name:'Computador',
+      name:'',
       price:'',
-      category:'eletronicos'
+      category:'1',
+      description:''
    });
   
-   const hanldeOnChange = (event :React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) =>{
+   const hanldeOnChange = (event :FormEvent) =>{
       const name = event.target.name;
       const value = event.target.value;
       
@@ -26,8 +30,14 @@ const Form = () => {
    const handleSubmit = (event: React.FormEvent<HTMLFormElement>)=> {
       event.preventDefault();
 
-      console.log(formData);
-    
+      const payload = {
+         ...formData,
+         imgUrl:'https://a-static.mlcdn.com.br/1500x1500/console-xbox-360-4gb-2-controles-wireless-microsoft/comercialfkl/369/15adbdeceaa41972b111e5117caeb994.jpg',
+         categories:[{id: formData.category}]
+      }
+         makeRequest({url: '/products', method:'POST', data: payload}).then(()=>{
+            setFormData({name:'', category:'',price:'', description:''});
+         })
    }
    
  return (
@@ -48,9 +58,9 @@ const Form = () => {
              className="form-control mb-5" onChange={hanldeOnChange}
              name="category"
              >
-               <option value="livros">Livros</option>
-               <option value="computadores">Computadores</option>
-               <option value="eletronicos">Eletronicos</option>
+               <option value="1">Livros</option>
+               <option value="3">Computadores</option>
+               <option value="2">Eletronicos</option>
             </select>
             <input
             value={formData.price}
@@ -60,6 +70,16 @@ const Form = () => {
             onChange={hanldeOnChange}
             placeholder="Preço"
             />
+         </div>
+         <div className="col-6">
+         <textarea 
+            name="description" 
+            value={formData.description}
+            onChange={hanldeOnChange}
+            className="form-control"
+            cols={30}
+            rows={10} 
+         />
          </div>
       </div>
     </BaseForm>
