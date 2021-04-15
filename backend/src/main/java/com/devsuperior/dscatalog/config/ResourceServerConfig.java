5d3678de2387371c6d69d856f19317fa7d0d4be5
JoelMaciel <soldado25461@gmail.com>
2021-04-15 01:47:22 -0300

    H2 security configuration

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -1,6 +1,9 @@
 package com.devsuperior.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -12,11 +15,14 @@ import org.springframework.stereotype.Component;
 @Component
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+	
+	@Autowired
+	private Environment env;
 
 	@Autowired
 	private JwtTokenStore tokenStore;
 
-	private static final String[] PUBLIC = { "/oauth/token" };
+	private static final String[] PUBLIC = { "/oauth/token","/h2-console/**" };
 
 	private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
 
@@ -29,10 +35,16 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-
-		http.authorizeRequests().antMatchers(PUBLIC).permitAll().antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN)
-				.permitAll().antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN").antMatchers(ADMIN)
-				.hasRole("ADMIN").anyRequest().authenticated();
+		//H2 liberar
+	   if( Arrays.asList(env.getActiveProfiles()).contains("test")) {
+		   http.headers().frameOptions().disable();
+	   }
+		
+		http.authorizeRequests().antMatchers(PUBLIC)
+		.permitAll().antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN)
+		.permitAll().antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN")
+		.antMatchers(ADMIN)
+		.hasRole("ADMIN").anyRequest().authenticated();
 
 	}
 
